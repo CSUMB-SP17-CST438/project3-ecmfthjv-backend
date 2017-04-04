5e2fa40098a48e1bd36544cfb1b95edfafcb5ff8
Louie Cortez <ernestoluiscortez@gmail.com>
2017-04-03 22:58:12 -0700

    Bug fix for firebase connection.  Connection will require an accountKey JSON file

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -3,12 +3,12 @@ var jwt = require('jsonwebtoken');
 // Initialize Firebase Admin
 var admin = require('firebase-admin');
 var config = require('../../config');
+var serviceAccountKey = require('../../serviceAccountKey');
 
 admin.initializeApp({
-  credential: admin.credential.cert(config.firebaseAdmin),
+  credential: admin.credential.cert(serviceAccountKey),
   databaseURL: "https://terrasite-58680.firebaseio.com"
 });
-
 // route to authenticate a user (POST http://localhost:8080/api/authenticate)
 router.post('/authenticate', function(req, res) {
   // Verify user is authenticated by Firebase
