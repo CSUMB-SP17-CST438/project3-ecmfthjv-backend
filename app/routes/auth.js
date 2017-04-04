b272fa8f2fe21e789c4fc7ab08d28cca0944fe9a
Louie Cortez <ernestoluiscortez@gmail.com>
2017-04-04 01:37:19 -0700

    Switch from UID to verifying id Token.

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -11,14 +11,12 @@ admin.initializeApp({
 // route to authenticate a user (POST http://localhost:8080/api/authenticate)
 router.post('/authenticate', function(req, res) {
   // Verify user is authenticated by Firebase
-  // TODO: Set this function to verifyIdToken instead of getUser
-  //admin.auth().verifyIdToken(req.body.idToken)
-  admin.auth().getUser(req.body.idToken)
+
+  admin.auth().verifyIdToken(req.body.idToken)
   .then(function(decodedToken) {
     var uid = decodedToken.uid;
     // create a token
     var token = jwt.sign(uid, config.secret);
-
     // return the information including token as JSON
     res.json({
       success: true,
