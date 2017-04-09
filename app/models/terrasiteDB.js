39d2b172a0831e0d061bbc26bb3446fd9fc9cbfc
Louie Cortez <ernestoluiscortez@gmail.com>
2017-04-09 11:17:20 -0700

    Add schema adjustments for double type

--- app/models/terrasiteDB.js
+++ app/models/terrasiteDB.js
@@ -1,13 +1,23 @@
 //app/models/terasiteDB.js
 
 var mongoose = require('mongoose');
+require('mongoose-double')(mongoose);
+
 var Schema = mongoose.Schema;
+var SchemaTypes = Schema.Types;
 
 var PostsSchema = new Schema({
     name: String,
-    longitude: String,//what's the best datatype for long/lat?
-    latitude: String,
-    content: String
+    longitude: {
+      type: SchemaTypes.Double
+    },
+    latitude: {
+      type: SchemaTypes.Double
+    },
+    altitude: {
+      type: SchemaTypes.Double
+    },
+    content: String,
 });
 
 module.exports = mongoose.model('ARposts', PostsSchema);
