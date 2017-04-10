538dac5b6305b858799cdeebfef30b66373edc35
Louie Cortez <ernestoluiscortez@gmail.com>
2017-04-09 23:20:08 -0700

    Add uid comparison to jwt payload

--- app/middleware/authVerification.js
+++ app/middleware/authVerification.js
@@ -6,16 +6,16 @@ module.exports = function(req, res, next) {
 
   // check header or url parameters or post parameters for token
   var token = req.body.token || req.query.token || req.headers['x-access-token'];
-  var idToken = req.body.idToken || req.query.idToken || req.headers['x-access-idToken'];
+  var idToken = req.body.idToken || req.query.idToken || req.headers['idtoken'];
   // decode token
   if (token) {
-
     // verifies secret and checks exp
     admin.auth().verifyIdToken(idToken)
     .then(function(decodedToken) {
       var uid = decodedToken.uid;
-      jwt.verify(token, app.get(uid, config.secret), function(err, decoded) {
-        if (err) {
+
+      jwt.verify(token, config.secret, function(err, decoded) {
+        if (err || uid !== decoded) {
           return res.json({ success: false, message: 'Failed to authenticate token.' });
         } else {
           // if everything is good, save to request for use in other routes
